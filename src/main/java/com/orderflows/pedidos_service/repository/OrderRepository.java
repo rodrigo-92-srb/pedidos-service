8b3870f39697185edff0836dcc1da0e80a5e8f00
rodrigosrb <rodrigo.92.srb@gmail.com>
2026-06-15 22:23:51 -0300

    chore: creating database and entity Order

---
+++ src/main/java/com/orderflows/pedidos_service/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.orderflows.pedidos_service.repository;
+
+import com.orderflows.pedidos_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
