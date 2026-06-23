d01943f206d118688cb573937835c92bde4a2552
rodrigosrb <rodrigo.92.srb@gmail.com>
2026-06-23 08:05:28 -0300

    Revert "Merge pull request #2 from rodrigo-92-srb/feat/core-application"
    This reverts commit 16b74727fceab65d6a651cf6b9450b4f1fbc25e8, reversing
    changes made to dd7534de70d0692f4873f53e4f1177ee162b59eb.

--- src/main/java/com/orderflows/pedidos_service/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.orderflows.pedidos_service.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/orderflows/pedidos_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orderflows.pedidos_service.repository;
-
-import com.orderflows.pedidos_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
